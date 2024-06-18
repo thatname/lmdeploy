4f9d14ec83e2fc76bf3d9eeccd74b4170c6b36f5
ZHOU He <461607353@qq.com>
2024-06-18 23:12:54 +0800

    Fix tensor parallel on Windows.

--- src/turbomind/models/llama/Barrier.h
+++ src/turbomind/models/llama/Barrier.h
@@ -6,12 +6,40 @@
 #include "src/turbomind/utils/logger.h"
 #ifndef _MSC_VER
 #include <pthread.h>
+#elif __cplusplus >= 202002L
+#include <barrier>
 #endif
 
 namespace turbomind {
 
 #ifdef _MSC_VER
 
+# if __cplusplus >= 202002L
+class Barrier {
+public:
+    Barrier(unsigned count) : barrier_(count)
+    {
+        TM_LOG_INFO("Barrier(%d)", (int)count);
+    }
+
+    Barrier(const Barrier&) = delete;
+    Barrier& operator=(const Barrier&) = delete;
+    Barrier(Barrier&&) noexcept        = delete;
+    Barrier& operator=(Barrier&&) noexcept = delete;
+
+    void wait() 
+	{
+        if (count_ > 1) {
+            barrier_.arrive_and_wait();
+        }		
+	}
+
+    ~Barrier() {}
+private:
+    const int         count_;
+    std::barrier      barrier_;
+};
+# else
 class Barrier {
 public:
     Barrier(unsigned count)
@@ -29,6 +57,7 @@ public:
 
     ~Barrier() {}
 };
+# endif
 
 #else
 
